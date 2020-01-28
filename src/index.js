f9d4c155a7ec545f7a49c3a2b51de34f75a7ee26
CristoferPortela <cristoferportela@gmail.com>
2020-01-28 19:55:43 -0300

    all the route code out and put on its  right folder

--- src/index.js
+++ src/index.js
@@ -1,60 +1,30 @@
-const express = require('express')
-const bp = require('body-parser')
+'use strict'
+// the express app function
+const app = require('express')()
+// used for make easier to separete the application in modules
+const consign = require('consign')
+// requiring and configuring mongodb
 const mongo = require('mongodb').MongoClient
-const ObjectID = require('mongodb').ObjectID
-
-const app = express()
-
 const conn = "mongodb://localhost:27017"
 
-app.set('view engine', 'ejs')
-
-app.use(bp.urlencoded({ extended: true }))
-
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
-
-app.get('/employers', (req, res) => {
-    db.collection('futureEmployers').find().toArray((err, results) => {
-        if (err) return console.log(err)
-        res.render('employers.ejs', { data: results })
-    })
-})
-
-app.route('/employers/:id')
-.get((req, res) => {
-    let id = req.params.id
-    db.collection('futureEmployers').find({_id: ObjectID(id)}).toArray((err, results) => {
-        if (err) return res.send(err)
-        res.render('employer.ejs', { data: results })
-    })  
-})
-.post((req, res) => {
-    let id = req.params.id
-    console.log(req.body)
-    db.collection('futureEmployers').updateOne({_id: ObjectID(id)}, {$set: req.body}, (err, results) => {
-        if (err) return console.log(err)
-        console.log(req.body)
-        res.redirect('/')
-    })  
-})
-
-app.post('/register', (req, res) => {
-    
-    db.collection('futureEmployers').insertOne(req.body, (err, result) =>{
-        if (err) return console.log(err)
-
-        res.redirect('/')
-    })
-})
+// connection with db
 
 mongo.connect(conn, (err, client) => {
     if (err) return console.log(err)
 
-    db = client.db('easyCrud')
+    // this will allow me to use the db connection in the consign autoload
+    const db = client.db('easyCrud')
+    app.db = db
 
-    app.listen(1414, () => {
-        console.log("Server running on 1414")
-    })
 })
+
+// the autoload with consign
+consign({ cwd: 'src' })
+    .then('middlewares')
+    .then('routes')
+    .into(app)
+
+
+const port = 2020
+
+app.listen(port, () => console.log(`Server running on ${port}`))
