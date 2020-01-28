76ed763d9f7d70ab27873488a886f1cfc6a42841
CristoferPortela <cristoferportela@gmail.com>
2020-01-28 20:48:05 -0300

    file more organized

--- src/middlewares/users.js
+++ src/middlewares/users.js
@@ -3,41 +3,64 @@ const ObjectID = require('mongodb').ObjectID
 
 module.exports = app => {
     
-    const find = (req, res) => {
+    const { emailValidate, notNull } = app.middlewares.exceptions
+
+    const index = (req, res) => {
         app.db.collection('futureEmployers').find().toArray((err, results) => {
             if (err) return console.log(err)
             res.json(results)
         })
     }
 
-    const getEmployers = (req, res) => {
+    const show = (req, res) => {
+
         let id = req.params.id
+
         app.db.collection('futureEmployers')
-            .find({_id: ObjectID(id)}).toArray((err, results) => {
+            .find({ _id: ObjectID(id) }).toArray((err, results) => {
                 if (err) return res.send(err)
                 res.json(results)
-            })  
+            })
     }
 
-    const postEmployers = (req, res) => {
+    const edit = (req, res) => {
+
         let id = req.params.id
-        console.log(req.body)
+
         app.db.collection('futureEmployers')
-            .updateOne({_id: ObjectID(id)}, {$set: req.body}, (err, results) => {
+            .updateOne({ _id: ObjectID(id) }, {$set: req.body}, (err, result) => {
+
                 if (err) return console.log(err)
 
-                res.redirect('/')
+                res.redirect(`/employers/${ id }`)
             })  
     }
 
-    const register = (req, res) => {
+    const store = (req, res) => {
 
-        app.db.collection('futureEmployers').insertOne(req.body, (err, result) =>{
+        try {
+            emailValidate(req.body.email, "Email incorreto")
+            notNull(req.body.name, "Nome não citado")
+            notNull(req.body.field, "Campo de trabalho desejado não citado")
+            notNull(req.body.name, "Por favor, envie o curriculo")
+        } catch (e) {
+            return res.status(400).send(e)
+        }        
+
+        const store = {
+            name: req.body.name,
+            field: req.body.field,
+            email: req.body.email,
+            tel: req.body.tel,
+            curriculum: req.body.curriculum
+        }
+
+        app.db.collection('futureEmployers').insertOne(store, (err, result) =>{
             if (err) return console.log(err)
     
-            res.redirect('/')
+            res.redirect(`/employers/${ result.ops[0]._id }`)
         })
     }
 
-    return { find, getEmployers, postEmployers, register }
+    return { index, show, edit, store }
 }
