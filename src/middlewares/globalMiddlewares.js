99ee1748908af5963bde993298b27eb27ab51760
CristoferPortela <cristoferportela@gmail.com>
2020-01-28 19:55:04 -0300

    global error on json fixed

---
+++ src/middlewares/globalMiddlewares.js
@@ -0,0 +1,15 @@
+'use strict'
+const bp = require('body-parser')
+const cors = require('cors')
+
+/**
+ *  Here we will hold the general middleware features
+ *  they will be used in ALL the routes
+ */
+
+module.exports = app => {
+    
+    app.use(bp.urlencoded({ extended: true }))
+    app.use(bp.json())
+    app.use(cors())
+}
